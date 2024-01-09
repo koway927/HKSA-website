ed5c8f1c130ea192f03c6f0225c2533dd653a1ec
koway927 <77182820+koway927@users.noreply.github.com>
2024-01-08 22:17:58 -0600

    Update board page; board and event page responsive

--- src/Board.js
+++ src/Board.js
@@ -1,87 +1,39 @@
 import React from 'react';
 import './Board.css';
 import { AiOutlineMail } from "react-icons/ai";
+import jsonData from './board_member_info.json';
+
+
+// Store the name, position, and email of each person in an array
+const people = jsonData.map(person => ({
+    name: person.Name,
+    position: person.Position,
+    email: person.Schoolemail
+}));
+  
+console.log(people);
 
 function Board() {
     return (
         <div className="Board">
             <div id = "board-background">
-                <img id = "board-picture" alt="" src="/meet_our_board_picture.png"/>
-                <h1>Meet Our Board</h1>
+                <img id = "board-group-picture" alt="" src="/meet_our_board_picture.png"/>
+                <h1 id = "board-background-title">Meet Our Board</h1>
             </div>
+
             <div id = "board-members-container">
-                <div class="board-row">
-                    <div class="board-column">
-                        <div class="board-card">
-                            <img class="board-picture"src="unknown_person.jpg" alt="unknown_person.jpg"></img>
-                            <div class="board-info-container">
-                                <h3 class="board-member-name"> Nicholas Yeung'25</h3>
-                                <p class="board-member-title">President</p>
-                                <a class="board-embedded-email" href="mailto:yinokny2@illinois.edu"><AiOutlineMail className = "email_icon" size ={30}></AiOutlineMail></a>
-                                
-                            </div>
+                {people.map((person, index) => (
+                    <div key={index} className = "board-card">
+                        <img className = "board-picture" src="unknown_person.jpg" alt="unknown_person.jpg" />
+                        <div className = "board-info-container">
+                            <h3 className = "board-member-name">{person.name}</h3>
+                            <p className = "board-member-position">{person.position}</p>
+                            <a className = "board-embedded-email" href={`mailto:${person.email}`}>
+                            <AiOutlineMail className="email_icon" size={30} />
+                            </a>
                         </div>
                     </div>
-
-                    <div class="board-column">
-                        <div class="board-card">
-                            <img class="board-picture" src="unknown_person.jpg" alt="unknown_person.jpg"></img>
-                            <div class="board-info-container">
-                                <h3 class="board-member-name">Marcus Fung'26</h3>
-                                <p class="board-member-title">Vice-President Internal</p>
-                                <a class="board-embedded-email" href="mailto:ylfung2@illinois.edu"><AiOutlineMail className = "email_icon" size ={30}></AiOutlineMail></a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="board-column">
-                        <div class="board-card">
-                            <img class="board-picture" src="unknown_person.jpg" alt="unknown_person.jpg"></img>
-                            <div class="board-info-container">
-                                <h3 class="board-member-name">Felicity Lo'26</h3>
-                                <p class="board-member-title">Vice-President External</p>
-                                <a class="board-embedded-email" href="mailto:fklo2@illinois.edu"><AiOutlineMail className = "email_icon" size ={30}></AiOutlineMail></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="board-row">
-                    <div class="board-column">
-                        <div class="board-card">
-                            <img class="board-picture"src="unknown_person.jpg" alt="unknown_person.jpg"></img>
-                            <div class="board-info-container">
-                                <h3 class="board-member-name"> Nicholas Yeung'25</h3>
-                                <p class="board-member-title">President</p>
-                                <a class="board-embedded-email" href="mailto:yinokny2@illinois.edu"><AiOutlineMail className = "email_icon" size ={30}></AiOutlineMail></a>
-                                
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="board-column">
-                        <div class="board-card">
-                            <img class="board-picture" src="unknown_person.jpg" alt="unknown_person.jpg"></img>
-                            <div class="board-info-container">
-                                <h3 class="board-member-name">Marcus Fung'26</h3>
-                                <p class="board-member-title">Vice-President Internal</p>
-                                <a class="board-embedded-email" href="mailto:ylfung2@illinois.edu"><AiOutlineMail className = "email_icon" size ={30}></AiOutlineMail></a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="board-column">
-                        <div class="board-card">
-                            <img class="board-picture" src="unknown_person.jpg" alt="unknown_person.jpg"></img>
-                            <div class="board-info-container">
-                                <h3 class="board-member-name">Felicity Lo'26</h3>
-                                <p class="board-member-title">Vice-President External</p>
-                                <a class="board-embedded-email" href="mailto:fklo2@illinois.edu"><AiOutlineMail className = "email_icon" size ={30}></AiOutlineMail></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
+                ))}
             </div>
         </div>
         
--- src/Event.js
+++ src/Event.js
@@ -1,7 +1,6 @@
 import React  from 'react';
 import './Event.css';
 
-
 function Events() {
     return (
         <div className="Event">
