2e6a9d97ed4f0afee629888f2c88a4af4840f27d
koway927 <77182820+koway927@users.noreply.github.com>
2024-01-14 20:22:16 -0600

    update the layout of board page

--- src/Board.js
+++ src/Board.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Board.css';
 import { AiOutlineMail } from "react-icons/ai";
-import jsonData from './board_member_info.json';
+import jsonData from './board_member_info/board_member_info.json';
 
-
+const board_section = ["Executive Board", "Intern", "Event Committee", "Fundraising Committee", "Social Committee"]
 // Store the name, position, and email of each person in an array
 const people = jsonData.map(person => ({
     name: person.Name,
     position: person.Position,
+    board_section: person.Boardsection,
     email: person.Schoolemail
 }));
 
@@ -19,16 +20,23 @@ function Board() {
                 <h1 id = "board-background-title">Meet Our Board</h1>
             </div>
 
-            <div id = "board-members-container">
-                {people.map((person, index) => (
-                    <div key={index} className = "board-card">
-                        <img className = "board-picture" src="unknown_person.jpg" alt="unknown_person.jpg" />
-                        <div className = "board-info-container">
-                            <h3 className = "board-member-name">{person.name}</h3>
-                            <p className = "board-member-position">{person.position}</p>
-                            <a className = "board-embedded-email" href={`mailto:${person.email}`}>
-                            <AiOutlineMail className="email_icon" size={30} />
-                            </a>
+            <div id = "board-container">
+                {board_section.map((section, section_index) => (
+                    <div key={section_index} className="board-section-container">
+                        <h1 className = "board-section-title">{section}</h1>
+                        <div className = "board-section">
+                            {people.filter(person => person.board_section.includes(section)).map((person, member_index) => (
+                                <div key={member_index} className = "board-card">
+                                    <img className = "board-picture" src="unknown_person.jpg" alt="unknown_person.jpg" />
+                                    <div className = "board-info-container">
+                                        <h3 className = "board-member-name">{person.name}</h3>
+                                        <p className = "board-member-position">{person.position}</p>
+                                        <a className = "board-embedded-email" href={`mailto:${person.email}`}>
+                                        <AiOutlineMail className="email_icon" size={30} />
+                                        </a>
+                                    </div>
+                                </div>
+                            ))}
                         </div>
                     </div>
                 ))}
