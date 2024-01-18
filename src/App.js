94d3c26c3d0d62779d8fa69a2b7441c1bdc2d078
koway927 <77182820+koway927@users.noreply.github.com>
2024-01-18 17:38:17 -0600

    remove container

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter ,Routes, Route, Link,NavLink } from "react-router-dom";
 import React from 'react';
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import Container from "react-bootstrap/Container";
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import './Home.css';
