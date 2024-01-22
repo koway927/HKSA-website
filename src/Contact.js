56e803f8931002d13d81310e5c165eab4c15deb0
koway927 <77182820+koway927@users.noreply.github.com>
2024-01-22 15:42:33 -0600

    update font size

--- src/Contact.js
+++ src/Contact.js
@@ -16,36 +16,36 @@ function Contact() {
             <div id = "contact-method-section">
 
                     <div className = "method">
-                        <h2 className = "media-description">Join our Discord server for updates!</h2>
+                        <p className = "media-description">Join our Discord server for updates!</p>
                         <a id = "discord-icon-link" target="_blank" rel="noopener noreferrer" href="https://discord.gg/BC5FEnGCYs">
                             <FaDiscord className = "icon" size={170}></FaDiscord>
                         </a>
                         <div id = "discord-text-link-container">
                             <a id = "discord-text-link" target="_blank" rel="noopener noreferrer" href="https://discord.gg/BC5FEnGCYs">
-                                <h2 className = "media-description">Discord Server</h2>
+                                <p className = "media-description">Discord Server</p>
                             </a>
                         </div>
                     </div>
                     
                     <div className = "method">
-                    <h2 className = "media-description">Reach out to us with any questions!</h2>
+                    <p className = "media-description">Reach out to us with any questions!</p>
                         <a id = "email-icon-link" href="mailto:hksa.uofi@gmail.com">
                             <AiOutlineMail className = "icon" size={170}></AiOutlineMail>
                         </a>
                         <a id = "email-text-link" href="mailto:hksa.uofi@gmail.com">
-                            <h2 className = "media-description">hksa.uofi@gmail.com</h2>
+                            <p className = "media-description">hksa.uofi@gmail.com</p>
                         </a>
                     </div>
   
  
                     <div className = "method">
-                        <h2 className = "media-description">Follow us on Instagram!</h2>
+                        <p className = "media-description">Follow us on Instagram!</p>
                         <a id = "instagram-icon-link" target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/uiuc.hksa?utm_source=ig_web_button_share_sheet&igsh=OGQ5ZDc2ODk2ZA==">
                             <FaInstagram className = "icon" size={170}  ></FaInstagram>
                         </a>
                         <div id = "instagram-text-link-container">
                             <a id = "instagram-text-link" target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/uiuc.hksa?utm_source=ig_web_button_share_sheet&igsh=OGQ5ZDc2ODk2ZA==">
-                                <h2 className = "media-description">@uiuc.hksa</h2>
+                                <p className = "media-description">@uiuc.hksa</p>
                             </a>
                         </div>                     
                     </div>
@@ -53,12 +53,12 @@ function Contact() {
                     <InstagramEmbed id = "instagram-page" url="https://www.instagram.com/uiuc.hksa/"/>
 
                     <div className = "method">
-                        <h2 className = "media-description">Subscribe to our biweekly newsletter!</h2>
+                        <p className = "media-description">Subscribe to our biweekly newsletter!</p>
                         <a id = "newsletter-icon-link" target="_blank" rel="noopener noreferrer" href="https://gmail.us8.list-manage.com/subscribe?u=34595721dc9c61017bba4ffcd&id=1a25ff137d">
                             <GiLoveLetter className = "icon" size={170}  ></GiLoveLetter>
                         </a>
                         <a id = "newsletter-text-link" target="_blank" rel="noopener noreferrer" href="https://gmail.us8.list-manage.com/subscribe?u=34595721dc9c61017bba4ffcd&id=1a25ff137d">
-                            <h2 className = "media-description">Newsletter</h2>
+                            <p className = "media-description">Newsletter</p>
                         </a>
                         
                     </div>
