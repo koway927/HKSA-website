ee38d68c3dc43a49e69fb416d25a8d88eeb5766c
koway927 <77182820+koway927@users.noreply.github.com>
2025-02-15 14:57:57 -0600

    updated for spring 2025

--- src/Board.js
+++ src/Board.js
@@ -30,7 +30,7 @@ function Board() {
                         <div className = "board-section">
                             {people.filter(person => person.board_section.includes(section)).map((person, member_index) => (
                                 <div key={member_index} className = "board-card">
-                                    <img className = "board-picture" src= {`/board_pictures/${person.name}.jpg`} onError={(e)=>{e.target.onerror = null; e.target.src=`/board_pictures/no_image_available.jpg`}}/>
+                                    <img className = "board-picture"  alt={""} src= {`/board_pictures/${person.name}.jpg`} onError={(e)=>{e.target.onerror = null; e.target.src=`/board_pictures/no_image_available.jpg`}}/>
                                     <div className = "board-info-container">
                                         <h3 className = "board-member-name">{person.name}</h3>
                                         <p className = "board-member-position">{person.position}</p>
